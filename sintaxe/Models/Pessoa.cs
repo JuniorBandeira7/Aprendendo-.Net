812e0059e1e9b3888f0ed954c370725f3799292c
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-08-15 19:21:52 -0300

    k

--- sintaxe/Models/Pessoa.cs
+++ sintaxe/Models/Pessoa.cs
@@ -9,7 +9,7 @@ namespace sintaxe.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
-
+        // Void quando retornar nada
         public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
--- sintaxe/obj/Debug/net8.0/sintaxe.AssemblyInfo.cs
+++ sintaxe/obj/Debug/net8.0/sintaxe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sintaxe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ba78fb0df4635ee07009a5ede563ea0a9ebf3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9471e50388d2e0613db1593e4dc5fd77c22ba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("sintaxe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sintaxe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
