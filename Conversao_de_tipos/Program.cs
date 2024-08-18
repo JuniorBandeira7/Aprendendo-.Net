0170dd40a301a1cad3b06ffe6786a079662f5d85
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-08-18 18:05:04 -0300

    .

---
+++ Conversao_de_tipos/Program.cs
@@ -0,0 +1,14 @@
+﻿// O parse dá erro se passar null
+int a = int.Parse("5");
+
+// O convart converte null para zero
+int b = Convert.ToInt32(null);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+string c = Convert.ToString(a);
+
+
+
+
